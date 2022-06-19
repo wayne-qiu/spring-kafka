b222b9eb87730f5165f6706ec886518019a4114d
wayneq05 <wei_qiu@hotmail.com>
2022-06-19 15:17:31 -0400

    Initial commit

---
+++ src/main/java/com/atome/springkafka/kafkatemplate/SpringKafkaApplication.java
@@ -0,0 +1,74 @@
+package com.atome.springkafka.kafkatemplate;
+
+import com.github.javafaker.Faker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaApplication.class, args);
+	}
+
+//	@Bean
+//	NewTopic hobbit2() {
+//		return TopicBuilder.name("hobbit2").partitions(15).replicas(3).build();
+//	}
+}
+
+@RequiredArgsConstructor
+@Component
+class Producer {
+
+	private final KafkaTemplate<Integer, String> template;
+
+	Faker faker;
+
+
+
+	@EventListener(ApplicationStartedEvent.class)
+	public void generate() {
+
+
+		faker = Faker.instance();
+		final Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
+
+		final Flux<String> quotes = Flux.fromStream(Stream.generate(() -> faker.hobbit().quote()));
+
+		Flux.zip(interval, quotes)
+				.map(it -> template.send("hobbit", faker.random().nextInt(42), it.getT2())).blockLast();
+
+		Flux.zip(interval, quotes)
+				.map(it -> template.send("hobbit", faker.random().nextInt(42), it.getT2()))
+				.blockLast();
+	}
+
+}
+
+@Component
+class Consumer {
+	@KafkaListener(topics= {"hobbit"}, groupId="spring-boot-kafka")
+	public void consume(String quote) {
+		System.out.println("received= " + quote);
+	}
+}
+
+//@Component
+//class Consumer {
+//
+//	@KafkaListener(topics = {"streams-wordcount-output"}, groupId = "spring-boot-kafka")
+//	public void consume(ConsumerRecord<String, Long> record) {
+//		System.out.println("received = " + record.value() + " with key " + record.key());
+//	}
+//}
